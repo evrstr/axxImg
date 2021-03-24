193b70be0195843764ff0f8ad2f586d38416f0be
evrstr <429427989yiqi@gmail.com>
2021-03-24 16:53:40 +0800

    d

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
 
 export default function (req: VercelRequest, res: VercelResponse) {
+    console.log(process.env.testEnv)
     const { name = 'World' } = req.query
     res.send(`Hello ${name}!`)
 }
