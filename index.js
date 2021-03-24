6a1bc22e98d99d7ed380db0d3a1924a44aad2d7f
evrstr <429427989yiqi@gmail.com>
2021-03-24 13:54:28 +0800

    d

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 
 
 module.exports = (req, res) => {
-    res.json({
-      body: req.body,
-      query: req.query,
-      cookies: req.cookies,
-    })
-  }
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  })
+  res.status(200).send("hellow world!")
+}
